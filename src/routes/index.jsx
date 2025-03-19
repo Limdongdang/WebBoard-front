94573909b240e8de1f36e84ff94038c89a515639
LimDong <50188317+Limdongdang@users.noreply.github.com>
2025-03-19 16:11:08 +0900

    board 세팅

---
+++ src/routes/index.jsx
@@ -0,0 +1,12 @@
+// index.jsx
+import React from 'react';
+import MyChessBoard from "../components/MyChessBoard";
+
+const routes = [
+  {
+    path: '/',
+    element: <MyChessBoard />
+  },
+];
+
+export default routes;
