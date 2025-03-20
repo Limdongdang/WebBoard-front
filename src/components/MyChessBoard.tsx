58e73e04dffaf38b7b47c98704b8e74c666faa28
LimDong <50188317+Limdongdang@users.noreply.github.com>
2025-03-20 18:57:24 +0900

    chess logic update

--- src/components/MyChessBoard.tsx
+++ src/components/MyChessBoard.tsx
@@ -1,10 +1,56 @@
-import React from 'react';
+import { Chess, Piece, Square } from 'chess.js';
+import React, { useState } from 'react';
 import { Chessboard } from 'react-chessboard';
 
 const MyChessBoard: React.FC = () => {
+  const [game, setGame] = useState(new Chess());
+  const [myColor, setMyColor] = useState<"white" | "black">("white");
+
+  function onDrop(sourceSquare: Square, targetSquare: Square) {
+    const gameCopy = new Chess(game.fen());
+    
+    const validMoves = gameCopy.moves({ square: sourceSquare, verbose: true });
+    const isValidMove = validMoves.some(
+      (move) => move.from === sourceSquare && move.to === targetSquare
+    );
+
+    if(!isValidMove) {
+      console.log("Invalid move");
+      return false;
+    }
+
+    try {
+      const move = gameCopy.move({
+        from: sourceSquare,
+        to: targetSquare,
+        promotion: "q", // 항상 퀸으로 승진
+      });
+  
+      if (move === null) {
+        console.log("Invalid move");
+        return false;
+      }
+  
+      setGame(gameCopy); // 상태 업데이트
+      console.log("Move made: ", move);
+      return true;
+    } catch (error) {
+      console.error("Error during move:", error);
+      return false;
+    }
+  }
+
   return (
     <div>
-      <Chessboard id="BasicBoard"/>
+      <Chessboard 
+       id="BasicBoard" 
+       position={game.fen()} 
+       onPieceDrop={onDrop}
+       boardOrientation={myColor}
+       areArrowsAllowed={true}
+       arePremovesAllowed={true}
+       allowDragOutsideBoard={false}
+      />
     </div>
   );
 };
